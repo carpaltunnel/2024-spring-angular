324bc1dd938cdc8b3b546504ba906ade3ac47db9
carpaltunnel <carpaltunnel@carpaltunnel.dev>
2024-05-06 21:00:26 -0500

    initial commit with wonky bootstrap

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'This Awesome Angular Project';
+  welcomeMessage = '... isn\'t this cool?'
+}
