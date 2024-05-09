f579633f8a7e5bcc6a462af14b5947a8d3779553
carpaltunnel <carpaltunnel@carpaltunnel.dev>
2024-05-08 20:58:28 -0500

    style changes and show/hide selected widget

--- src/app/widgets/widgets.component.ts
+++ src/app/widgets/widgets.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { Widget } from '../widget';
-import { NgFor, UpperCasePipe } from '@angular/common';
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { WIDGETS } from '../mock-widgets';
 
 @Component({
   selector: 'app-widgets',
   standalone: true,
-  imports: [UpperCasePipe, FormsModule, NgFor],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgIf],
   templateUrl: './widgets.component.html',
   styleUrl: './widgets.component.css'
 })
 export class WidgetsComponent {
-  selectedWidget: Widget = {} as Widget;
+  selectedWidget!: Widget;
 
   widgets = WIDGETS;
 
