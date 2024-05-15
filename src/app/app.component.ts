cfdb6d411d1bd7947309648437e203a6613b9f38
carpaltunnel <carpaltunnel@carpaltunnel.dev>
2024-05-15 18:07:06 -0500

    initial routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { WidgetsComponent } from './widgets/widgets.component';
 import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, WidgetsComponent, MessagesComponent],
+  imports: [RouterOutlet, RouterLink, WidgetsComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { WidgetsComponent } from './widgets/widgets.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'widgets', component: WidgetsComponent }
+];
