33ff8507af60c4e910690e66e61726c25655745f
carpaltunnel <carpaltunnel@carpaltunnel.dev>
2024-06-10 21:02:15 -0500

    basic auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 import { WidgetsComponent } from './widgets/widgets.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, WidgetsComponent, MessagesComponent],
+  imports: [NgIf, RouterOutlet, RouterLink, WidgetsComponent, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'This Awesome Angular Project';
   welcomeMessage = '... isn\'t this cool?';
+
+  constructor(public authService: AuthService, private router: Router) {};
+
+  logout = () => {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
